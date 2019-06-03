8c5a2680a1b445eb34d162882e787ccf37be3dc3
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2019-06-03 20:53:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const pirse= member.guild.channels.get("إيدي الروم حق السيرفر الي ترحب فيه");
+if(!pirse) return;
+if(pirse) {
+setTimeout(() => pirse.send(`رسالة الولكمة هنآ`), 4000)        
+}
+});
+
+bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
